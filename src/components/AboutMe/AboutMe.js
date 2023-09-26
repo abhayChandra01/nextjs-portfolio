3d15e4452d96b2d203b321a49bf6640ff8724b18
Abhay Chandra <89189730+abhayChandra01@users.noreply.github.com>
2023-09-26 13:21:53 +0530

    Update AboutMe.js

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -6,7 +6,7 @@ const AboutMe = () => (
   <Section id='about-me'>
     <SectionTitle>A bit about me</SectionTitle>
     <SectionText>
-      I belong to Shahjahanpur, Uttar Pradesh. I did my schooling from CBSE Board & currently, I am persuing Bachelor Of Technology from ITM Gwalior in the field of Information Technology. <br />
+      I belong to Shahjahanpur, Uttar Pradesh. I did my schooling from CBSE Board & completed my Bachelor Of Technology from ITM Gwalior in the field of Information Technology. <br />
       I am a passionate developer seeking a challenging position in a renowned organization that values learning, growth, and innovation, so that I can expand my skillset.<br />
       Proficient in React.js.<br />
       As a highly self-motivated web developer, I thrive on building web applications and continuously expanding my skills with the latest technologies to create meaningful and innovative solutions.
