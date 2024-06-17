bf42e4009d1e64ed325ba4caec8a7b475c3771dc
Abhay Chandra <a4abhaychandra@gmail.com>
2024-06-17 16:21:25 +0530

    new changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin, AiFillTwitterCircle } from 'react-icons/ai';
-import { Link } from '../../styles/GlobalComponents';
-import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import React from "react";
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiFillTwitterCircle,
+} from "react-icons/ai";
+import { Link } from "../../styles/GlobalComponents";
+import { SocialIcons } from "../Header/HeaderStyles";
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from "./FooterStyles";
 
 const Footer = () => {
   return (
@@ -10,25 +25,37 @@ const Footer = () => {
       <LinkList>
         <LinkColumn>
           <LinkTitle>Email</LinkTitle>
-          <LinkItem href="mailto:a4abhaychandra@gmail.com">
-            a4abhaychandra@gmail.com
+          <LinkItem href="mailto:abhaychandra.work@gmail.com">
+            abhaychandra.work@gmail.com
           </LinkItem>
         </LinkColumn>
       </LinkList>
       <SocialIconsContainer>
         <CompanyContainer>
-          <Slogan>&copy; 2022 <Link target="_blank" href="https://www.linkedin.com/in/abhay-chandra">  Abhay Chandra</Link> . All rights reserved.</Slogan>
+          <Slogan>
+            &copy; 2022{" "}
+            <Link
+              target="_blank"
+              href="https://www.linkedin.com/in/abhay-chandra"
+            >
+              {" "}
+              Abhay Chandra
+            </Link>{" "}
+            . All rights reserved.
+          </Slogan>
         </CompanyContainer>
         <SocialContainer>
           <SocialIcons target="_blank" href="https://github.com/abhayChandra01">
             <AiFillGithub size="3rem" />
           </SocialIcons>
-          <SocialIcons target="_blank" href="https://www.linkedin.com/in/abhay-chandra/">
+          <SocialIcons
+            target="_blank"
+            href="https://www.linkedin.com/in/abhay-chandra/"
+          >
             <AiFillLinkedin size="3rem" />
           </SocialIcons>
         </SocialContainer>
       </SocialIconsContainer>
-
     </FooterWrapper>
   );
 };
